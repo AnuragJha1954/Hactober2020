1aebbf745f3a14cdc05beba84e36c385b708b515
anshmelo <56475119+anshmelo@users.noreply.github.com>
2020-10-01 22:53:12 +0530

    Update helll.py

--- helll.py
+++ helll.py
@@ -36,3 +36,10 @@ sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)  
 print('The solution are {0} and {1}'.format(sol1,sol2)) 
 
+#added feature string reversal
+def my_function(x):
+  return x[::-1]
+
+mytxt = my_function("I wonder how this text looks like backwards")
+
+print(mytxt)
